5d256aa55ee0cc47dfc2f648e59fc7e3fab5e645
AnaFedalto <ana.fedalto@escola.pr.gov.br>
2025-07-30 08:02:32 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Karatê',
+    'Qual o tempo de uma luta adulta?',
+    'Uma luta adulta tem tempo de 3 minutos pausados'
+)
+
+criaCartao(
+    'Karate',
+    'O que é o kata?',
+    'É uma sequência pré-definida de movimentos que simulam um combate contra oponentes
+                        imaginários.
+                        É uma forma de treinamento que visa aprimorar a técnica, a postura, a coordenação e o foco,
+                        além de preservar a história e a filosofia da arte marcial.'
+)
+
+criaCartao(
+'Karate',
+'Onde surgiu o karatê? ',
+'O karatê surgiu na ilha de Okinawa, no Japão'
+
+)
