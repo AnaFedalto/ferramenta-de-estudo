752e5c3a998082ac33441496a33a5ff6879e3587
AnaFedalto <ana.fedalto@escola.pr.gov.br>
2025-08-13 07:34:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) {
-  console.log(categoria, pergunta, resposta)
+ let container= document.getElementById ('container')
+  let cartao = document.createElement('artcle')
+  cartao..className = 'cartao'
+
+  cartao.innerHTML= '
 }
