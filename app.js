3c386f00b388e66990de5bf5dc502015c91de488
AnaFedalto <ana.fedalto@escola.pr.gov.br>
2025-08-13 07:37:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ function criaCartao(categoria, pergunta, resposta) {
   cartao..className = 'cartao'
 
   cartao.innerHTML= `
+   <div class="cartao__conteudo">
+                    <h3>Karate</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p> Onde surgiu o karatê? </p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O karatê surgiu na ilha de Okinawa, no Japão</p>
+                    </div>
+                </div>
+                `
 }
